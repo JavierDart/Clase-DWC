5e874de6031641f303fa6262cb0e0c84354c11cd
JavierDart <javinico08@gmail.com>
2017-09-28 11:21:55 +0200

    .

---
+++ EjemploFuncion.js
@@ -0,0 +1,24 @@
+// //funcion simple
+// function saludo(){
+// console.log("hola");
+// }
+// saludo();
+//
+// ********************************
+//una forma de hacerlo:
+// function saludo(){
+// console.log("hola");
+// return x;
+// var x =4;
+//
+// }
+//
+
+
+//Hay que seguir este orden:
+function saludo(){
+  var x =4;
+  return x;
+  console.log("hola");
+}
+var salida = saludo();
